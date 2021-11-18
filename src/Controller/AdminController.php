0e4bf8c52de14fcc5361df6662477b6b6fa0b323
JAG <jacek.gadomski@gmail.com>
2021-11-18 12:54:33 +0100

    add user roles restrictions

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -26,7 +26,7 @@ class AdminController extends AbstractController
 
 
     /**
-     * @Route("/categories", name="categories", methods={"GET", "POST"})
+     * @Route("/su/categories", name="categories", methods={"GET", "POST"})
      */
     public function categories(CategoryTreeAdminList $categories, Request $request): Response
     {
@@ -54,7 +54,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/edit-category/{id}", name="edit_category", methods={"GET", "POST"})
+     * @Route("/su/edit-category/{id}", name="edit_category", methods={"GET", "POST"})
      */
     public function editCategory(Category $category, Request $request): Response
     {
@@ -79,7 +79,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/delete-category/{id}", name="delete_category")
+     * @Route("/su/delete-category/{id}", name="delete_category")
      */
     public function deleteCategory(Category $category): Response
     {
@@ -98,7 +98,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/upload-video", name="upload_video")
+     * @Route("/su/upload-video", name="upload_video")
      */
     public function uploadVideo(): Response
     {
@@ -106,7 +106,7 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("/users", name="users")
+     * @Route("/su/users", name="users")
      */
     public function users(): Response
     {
@@ -115,6 +115,9 @@ class AdminController extends AbstractController
 
     public function getAllCategories(CategoryTreeAdminOptionList $categories, $editedCategory = null)
     {
+
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         $categories->getCategoryList($categories->buildTree());
         return $this->render('admin/_all_categories.html.twig',[
             'categories'=>$categories,
